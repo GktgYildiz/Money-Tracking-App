71e562e16064372e0a6f140ff622ccabce7d8eea
Kanayo <kanayo.uehara+github@gmail.com>
2023-08-28 15:27:59 -0700

    add balance calculator

--- frontend/src/js/components/Account.js
+++ frontend/src/js/components/Account.js
@@ -10,7 +10,8 @@ $(document).ready(async function () {
       apiClient.fetchAccounts(),
       apiClient.fetchTransactions(),
     ]);
-    // console.log(fetchedAccounts[]);
+    console.log('fetched account', fetchedAccounts);
+    // console.log('fetched transactions', fetchedTransactions);
     // console.log(fetchedCategories, fetchedAccounts, fetchedTransactions);
 
     // ==========================
@@ -24,12 +25,14 @@ $(document).ready(async function () {
         const account = await apiClient.createAccount(accountPayload);
         // $("#na-result").text("Your account created!: " + JSON.stringify(account));
 
+        // append the new account data to the account summery
         $('#as-table').append(`<tr id="as-account-${account.id}"></tr>`)
-        $(`#as-account-${account.id}`).append(`<td>${account.id}</td>`)
-        $(`#as-account-${account.id}`).append(`<td>${account.username}</td>`)
-        $(`#as-account-${account.id}`).append(`<td>0</td>`)
+        $(`#as-account-${account.id}`).append(`<td>${account.id}</td>`, `<td>${account.username}</td>`, `<td>CA$0.00</td>`)
+
+        // set tailwind styles
         setStyles()
 
+        // account created popup
         $('#na-popup p').text(`Account "${account.username}" has been created!`)
         $('#na-popup').fadeIn()
         setTimeout(() => {
@@ -39,7 +42,6 @@ $(document).ready(async function () {
 
         $("#na-new-account").val(null); // Reset input
         
-        // location.reload();
       } catch (error) {
         $("#na-result").text("An error occurred: " + error.message);
       }
@@ -51,18 +53,43 @@ $(document).ready(async function () {
     // Code here
 
     fetchedAccounts.forEach(element => {
+
+      // call calcBalance
+      let balance = calcBalance(element)
+      balance = balance.toLocaleString("en-US", {style:"currency", currency:"CAD"});
+      
       $('#as-table').append(`<tr id="as-account-${element.id}"></tr>`);
-      $(`#as-account-${element.id}`).append(`<td>${element.id}</td>`);
-      $(`#as-account-${element.id}`).append(`<td>${element.username}</td>`);
-      $(`#as-account-${element.id}`).append(`<td>0</td>`);
+      $(`#as-account-${element.id}`).append(`<td>${element.id}</td>`, `<td>${element.username}</td>`, `<td>${balance}</td>`);
+      
     });
 
     setStyles()
+
   } catch (err) {
     console.error(err);
   }
 
 });
+
+// calcrate balance 
+function calcBalance(account) {
+  let transactions = account.transactions
+  let sum = 0
+
+  transactions.forEach(transaction => {
+    if(transaction.type === "Deposit") {
+      sum += transaction.amount
+    } else if (transaction.type === "Withdraw") {
+      sum -= transaction.amount
+    } else if (transaction.type === "Transfer" && transaction.accountIdFrom === account.id) {
+      sum -= transaction.amount
+    } else if (transaction.type === "Transfer" && transaction.accountIdTo === account.id) {
+      sum += transaction.amount
+    }
+  });
+
+  return sum
+}
 
 // Add tailwind classes
 function setStyles() {
