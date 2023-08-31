9ca0f2e3830c31d7c9e7a178a92fd59f728c3777
Kanayo <kanayo.uehara+github@gmail.com>
2023-08-30 17:29:35 -0700

    add login page

---
+++ frontend/src/js/components/Login.js
@@ -0,0 +1,66 @@
+const loginUser = [
+  {
+    username: 'john',
+    password: 'ciccc'
+  },
+  {
+    username: 'kanayo',
+    password: 'ciccc'
+  },
+  {
+    username: 'shuya',
+    password: 'ciccc'
+  },
+  {
+    username: 'sky',
+    password: 'ciccc'
+  },
+  {
+    username: 'taiki',
+    password: 'ciccc'
+  }
+]
+
+$(document).ready(function() {
+  $('input').on('focus', function(){
+    $('#incorrect-msg').addClass('opacity-0')
+  })
+
+  $('#password-visible-icon').click(function(event){
+    let passwordStatus = $(this).attr('src')
+    if (passwordStatus === 'images/visibility.svg') {
+      $(this).attr('src', 'images/visibility_off.svg')
+      $('#password').attr('type', 'text')
+    } else {
+      $(this).attr('src', 'images/visibility.svg')
+      $('#password').attr('type', 'password')
+
+
+    }
+  })
+  
+
+  $('#login-btn').click(function (event) {
+    let inputUsername = $('#loginusername').val()
+    let inputPassword = $('#password').val()
+
+    let failCount = 0
+    loginUser.forEach(user => {
+      if(inputUsername === user.username &&
+        inputPassword === user.password) {
+          localStorage.setItem('username', inputUsername)
+          $(this).attr('href', 'index.html')
+          
+        } else {
+          failCount++
+
+        }
+    });
+
+    if(failCount === loginUser.length) {
+      event.preventDefault()
+      $('#incorrect-msg').removeClass('opacity-0')
+    }
+  })
+
+})
